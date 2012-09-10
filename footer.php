21092585e87e353c06bb925100bda21910744bdd
Clare Bayley <clareb@hattery.com>
2012-09-10 14:02:53 -0700

    1st commit - my attempt at a generic wp template

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div class="clear"></div>
+	
+<div id="footer">
+	<div class="container">
+	<?php wp_nav_menu( array('theme_location' => 'footer-menu')); ?> 
+		
+	</div><!--.container-->
+</div> <!--end footer-->
+	
+</div><!--end main-->
+
+<?php wp_footer(); ?>
+
+<!-- Don't forget analytics -->
+	
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<title><?php if ( is_category() ) {
+		echo 'Category Archive for &quot;'; single_cat_title(); echo '&quot; | '; bloginfo( 'name' );
+	} elseif ( is_tag() ) {
+		echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
+	} elseif ( is_archive() ) {
+		wp_title(''); echo ' Archive | '; bloginfo( 'name' );
+	} elseif ( is_search() ) {
+		echo 'Search for &quot;'.wp_specialchars($s).'&quot; | '; bloginfo( 'name' );
+	} elseif ( is_home() ) {
+		bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
+	}  elseif ( is_404() ) {
+		echo 'Error 404 Not Found | '; bloginfo( 'name' );
+	} elseif ( is_single() ) {
+		wp_title('');
+	} else {
+		echo wp_title(''); echo ' | '; bloginfo( 'name' );
+	} ?></title>
+	
+	<meta name="description" content="<?php wp_title(''); echo ' | '; bloginfo( 'description' ); ?>" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<meta name="viewport" content="width=device-width; initial-scale=1"/><?php /* Add "maximum-scale=1" to fix the Mobile Safari auto-zoom bug on orientation changes, but keep in mind that it will disable user-zooming completely. Bad for accessibility. */ ?>
+	
+	<link rel="icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" type="image/x-icon" />
+	
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/reset.css" />	
+		
+	<!--This includes the local stylesheet and the less file to translate it-->
+	<link rel="stylesheet/less" href="<?php echo bloginfo('template_directory'); ?>/style.less" type="text/css" />
+	<script src="<?php echo bloginfo('template_directory'); ?>/js/less-1.3.0.min.js" type="text/javascript"></script>	
+	
+	
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+	<link rel="alternate" type="application/atom+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'atom_url' ); ?>" />
+
+	<?php wp_enqueue_script("jquery"); /* Loads jQuery if it hasn't been loaded already */ ?>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<?php wp_head(); ?> <?php /* this is used by many Wordpress features and for plugins to work proporly */ ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+<div style="display: none;">
+	<p><a href="#content"><?php _e('Skip to Content'); ?></a></p><?php /* used for accessibility, particularly for screen reader applications */ ?>
+</div><!--.none-->
+
+<div id="main"><!-- this encompasses the entire Web site -->
+	<div id="header"><header>
+		<div class="container">
+			<?php wp_nav_menu( array('theme_location' => 'header-menu') ); ?>		
+		</div><!--.container-->
+	<div class="clear"></div>
+	</header></div><!--#header-->
